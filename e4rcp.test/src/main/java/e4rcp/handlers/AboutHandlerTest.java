404bd6158eb15143084b76710f4922e977fdd070
chengyi <t0963162429@gmail.com>
2013-12-29 13:09:31 +0800

    Init rcp project

---
+++ e4rcp.test/src/main/java/e4rcp/handlers/AboutHandlerTest.java
@@ -0,0 +1,14 @@
+package e4rcp.handlers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AboutHandlerTest {
+
+	@Test
+	public void testGetGreeting() {
+		assertTrue(true);
+	}
+
+}
